ebbb96f538bd236b3a52091e8343fd1ab996927c
Alex Campbell <alexbe.campbell@gmail.com>
2020-11-20 16:32:08 -0600

    initial commit, base mode finished

---
+++ server/routes/secrets.router.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
+
+// Unauthenticated
+router.get('/', rejectUnauthenticated, (req, res) => {
+  console.log('req.user:', req.user);
+  pool
+    .query(
+      `SELECT "secret".secrecy_level, "secret".content 
+      FROM  "secret" 
+      WHERE "secret".secrecy_level < ${req.user.clearance_level};`
+    )
+    .then((results) => res.send(results.rows))
+    .catch((error) => {
+      console.log('Error making SELECT for secrets:', error);
+      res.sendStatus(500);
+    });
+});
+
+module.exports = router;
